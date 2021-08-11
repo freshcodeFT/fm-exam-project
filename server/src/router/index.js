4b81ede1a96e8e88e7fc7049f51cf79d2c96e6b5
Nikolay <freshitftm@gmail.com>
2021-08-11 12:06:11 +0300

    Refactor router

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -10,7 +10,7 @@ const authRouter = require('./auth');
 const router = express.Router();
 
 router.use('/auth', authRouter);
-
+/*
 router.post(
   '/registration',
   validators.validateRegistrationData,
@@ -18,16 +18,14 @@ router.post(
 );
 
 router.post('/login', validators.validateLogin, userController.login);
+*/
 
-router.post(
-  '/dataForContest',
-  checkToken.checkToken,
-  contestController.dataForContest,
-);
+router.use(checkToken.checkToken);
+
+router.post('/dataForContest', contestController.dataForContest);
 
 router.post(
   '/pay',
-  checkToken.checkToken,
   basicMiddlewares.onlyForCustomer,
   upload.uploadContestFiles,
   basicMiddlewares.parseBody,
@@ -35,44 +33,32 @@ router.post(
   userController.payment,
 );
 
-router.post(
-  '/getCustomersContests',
-  checkToken.checkToken,
-  contestController.getCustomersContests,
-);
+router.post('/getCustomersContests', contestController.getCustomersContests);
 
 router.get(
   '/getContestById',
-  checkToken.checkToken,
   basicMiddlewares.canGetContest,
   contestController.getContestById,
 );
 
 router.post(
   '/getAllContests',
-  checkToken.checkToken,
   basicMiddlewares.onlyForCreative,
   contestController.getContests,
 );
 
 router.post('/getUser', checkToken.checkAuth);
 
-router.get(
-  '/downloadFile/:fileName',
-  checkToken.checkToken,
-  contestController.downloadFile,
-);
+router.get('/downloadFile/:fileName', contestController.downloadFile);
 
 router.post(
   '/updateContest',
-  checkToken.checkToken,
   upload.updateContestFile,
   contestController.updateContest,
 );
 
 router.post(
   '/setNewOffer',
-  checkToken.checkToken,
   upload.uploadLogoFiles,
   basicMiddlewares.canSendOffer,
   contestController.setNewOffer,
@@ -80,72 +66,44 @@ router.post(
 
 router.post(
   '/setOfferStatus',
-  checkToken.checkToken,
   basicMiddlewares.onlyForCustomerWhoCreateContest,
   contestController.setOfferStatus,
 );
 
 router.post(
   '/changeMark',
-  checkToken.checkToken,
   basicMiddlewares.onlyForCustomer,
   userController.changeMark,
 );
 
-router.post(
-  '/updateUser',
-  checkToken.checkToken,
-  upload.uploadAvatar,
-  userController.updateUser,
-);
+router.post('/updateUser', upload.uploadAvatar, userController.updateUser);
 
 router.post(
   '/cashout',
-  checkToken.checkToken,
   basicMiddlewares.onlyForCreative,
   userController.cashout,
 );
 
-router.post('/newMessage', checkToken.checkToken, chatController.addMessage);
+router.post('/newMessage', chatController.addMessage);
 
-router.post('/getChat', checkToken.checkToken, chatController.getChat);
+router.post('/getChat', chatController.getChat);
 
-router.post('/getPreview', checkToken.checkToken, chatController.getPreview);
+router.post('/getPreview', chatController.getPreview);
 
-router.post('/blackList', checkToken.checkToken, chatController.blackList);
+router.post('/blackList', chatController.blackList);
 
-router.post('/favorite', checkToken.checkToken, chatController.favoriteChat);
+router.post('/favorite', chatController.favoriteChat);
 
-router.post(
-  '/createCatalog',
-  checkToken.checkToken,
-  chatController.createCatalog,
-);
+router.post('/createCatalog', chatController.createCatalog);
 
-router.post(
-  '/updateNameCatalog',
-  checkToken.checkToken,
-  chatController.updateNameCatalog,
-);
+router.post('/updateNameCatalog', chatController.updateNameCatalog);
 
-router.post(
-  '/addNewChatToCatalog',
-  checkToken.checkToken,
-  chatController.addNewChatToCatalog,
-);
+router.post('/addNewChatToCatalog', chatController.addNewChatToCatalog);
 
-router.post(
-  '/removeChatFromCatalog',
-  checkToken.checkToken,
-  chatController.removeChatFromCatalog,
-);
+router.post('/removeChatFromCatalog', chatController.removeChatFromCatalog);
 
-router.post(
-  '/deleteCatalog',
-  checkToken.checkToken,
-  chatController.deleteCatalog,
-);
+router.post('/deleteCatalog', chatController.deleteCatalog);
 
-router.post('/getCatalogs', checkToken.checkToken, chatController.getCatalogs);
+router.post('/getCatalogs', chatController.getCatalogs);
 
 module.exports = router;
