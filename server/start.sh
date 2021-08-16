3d90d33117550d04f70d4e0b8b3a96ce8bdcb5a8
Nikolay <freshitftm@gmail.com>
2021-08-16 11:29:19 +0300

    Refactor Docker

---
+++ server/start.sh
@@ -0,0 +1,11 @@
+# Run Sequalize's migrations.
+echo "-----> Running application migrations"
+sequelize db:migrate
+echo "<----- Migrations created"
+
+# Run Sequalize's seeds.
+echo "-----> Running application seeds"
+sequelize db:seed:all
+echo "<----- Seeds created"
+
+npm run start
